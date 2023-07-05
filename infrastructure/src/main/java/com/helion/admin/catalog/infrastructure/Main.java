f6de25cb0034b3e407212f6b104045bffa1f3aa3
helionj <helionporto@hotmail.com>
2023-07-05 11:22:00 -0400

    Rabbitm Admin Config

--- infrastructure/src/main/java/com/helion/admin/catalog/infrastructure/Main.java
+++ infrastructure/src/main/java/com/helion/admin/catalog/infrastructure/Main.java
@@ -1,23 +1,11 @@
 package com.helion.admin.catalog.infrastructure;
 
-import com.helion.admin.catalog.application.UseCase;
-import com.helion.admin.catalog.application.category.create.CreateCategoryUseCase;
-import com.helion.admin.catalog.application.category.delete.DeleteCategoryUseCase;
-import com.helion.admin.catalog.application.category.retrieve.get.GetCategoryByIdUseCase;
-import com.helion.admin.catalog.application.category.retrieve.list.ListCategoriesUseCase;
-import com.helion.admin.catalog.application.category.update.UpdateCategoryUseCase;
-import com.helion.admin.catalog.domain.category.Category;
-import com.helion.admin.catalog.infrastructure.category.persistence.CategoryJpaEntity;
-import com.helion.admin.catalog.infrastructure.category.persistence.CategoryRepository;
 import com.helion.admin.catalog.infrastructure.configuration.WebServerConfig;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.AbstractEnvironment;
 
-import java.util.List;
 
 @SpringBootApplication
 public class Main {
@@ -26,5 +14,7 @@ public class Main {
         System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
     }
+
+   
 
 }
--- infrastructure/src/main/java/com/helion/admin/catalog/infrastructure/configuration/AmqpConfig.java
+++ infrastructure/src/main/java/com/helion/admin/catalog/infrastructure/configuration/AmqpConfig.java
@@ -2,7 +2,9 @@ package com.helion.admin.catalog.infrastructure.configuration;
 
 import com.helion.admin.catalog.infrastructure.configuration.annotations.VideoCreatedQueue;
 import com.helion.admin.catalog.infrastructure.configuration.annotations.VideoEncodedQueue;
+import com.helion.admin.catalog.infrastructure.configuration.annotations.VideoEvents;
 import com.helion.admin.catalog.infrastructure.configuration.properties.amqp.QueueProperties;
+import org.springframework.amqp.core.*;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,9 +26,46 @@ public class AmqpConfig {
         return new QueueProperties();
     }
 
-    @Bean
-    public String queueName(@VideoCreatedQueue QueueProperties props){
-        return props.getQueue();
+    @Configuration
+    static class Admin {
+
+        @Bean
+        @VideoEvents
+        public Exchange videoEventsExchange(@VideoCreatedQueue QueueProperties props){
+            return new DirectExchange(props.getExchange());
+        }
+
+        @Bean
+        @VideoCreatedQueue
+        public Queue videoCreatedQueue(@VideoCreatedQueue QueueProperties props){
+            return new Queue(props.getQueue());
+        }
+
+        @Bean
+        @VideoCreatedQueue
+        public Binding videoCreatedBinding(
+                @VideoEvents DirectExchange exchange,
+                @VideoCreatedQueue Queue queue,
+                @VideoCreatedQueue QueueProperties props
+        ){
+            return BindingBuilder.bind(queue).to(exchange).with(props.getRoutingKey());
+        }
+
+        @Bean
+        @VideoEncodedQueue
+        public Queue videoEncodedQueue(@VideoEncodedQueue QueueProperties props){
+            return new Queue(props.getQueue());
+        }
+
+        @Bean
+        @VideoEncodedQueue
+        public Binding videoEncodedBinding(
+                @VideoEvents DirectExchange exchange,
+                @VideoEncodedQueue Queue queue,
+                @VideoEncodedQueue QueueProperties props
+        ){
+            return BindingBuilder.bind(queue).to(exchange).with(props.getRoutingKey());
+        }
     }
 
 }
